4703abe62d6a5bb4f755f4e017f15c9b5b96edc0
EduCsg <eduardocasagrande71@gmail.com>
2022-08-13 22:54:13 -0300

    Desafio Laço For

---
+++ dart/controle/desafio_for.dart
@@ -0,0 +1,20 @@
+main() {
+  print('Minha solução\n');
+
+  //minha solução
+  var hashtags = ['#', '##', '###', '####', '#####', '######'];
+
+  for (var variavel in hashtags) {
+    print(variavel);
+  }
+
+  //
+  //
+  //
+  print('\nSolução do professor \n');
+
+  //solução do professor
+  for (var valor = '#'; valor != '#######'; valor += '#') {
+    print(valor);
+  }
+}
