97f476dcff3019ac7f6f4685edaca39006cd2432
EduCsg <eduardocasagrande71@gmail.com>
2022-08-15 23:07:03 -0300

    Classes e métodos

---
+++ dart/Orient_Obj/classe_data.dart
@@ -0,0 +1,35 @@
+class Data {
+  int? dia;
+  int? mes;
+  int? ano;
+
+  String obterFormatada() {
+    return "$dia/$mes/$ano";
+  }
+
+  String toString() {
+    return obterFormatada();
+  }
+}
+
+main() {
+  var dataAniversario = new Data();
+  dataAniversario.dia = 3;
+  dataAniversario.mes = 10;
+  dataAniversario.ano = 2020;
+
+  Data dataCompra = Data();
+  dataCompra.dia = 23;
+  dataCompra.mes = 12;
+  dataCompra.ano = 2021;
+
+  // print("${dataAniversario.dia}/${dataAniversario.mes}/${dataAniversario.ano}");
+  // print("${dataCompra.dia}/${dataCompra.mes}/${dataCompra.ano}");
+  String d1 = dataAniversario.obterFormatada();
+
+  print("A data do aniversário é $d1");
+  print("A data da compra é ${dataCompra.obterFormatada()}");
+
+  print(dataCompra);
+  print(dataCompra.toString());
+}
