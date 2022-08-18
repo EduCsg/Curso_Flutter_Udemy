3db7bc4d39e923e9b4b0349698690833cf61e20f
EduCsg <eduardocasagrande71@gmail.com>
2022-08-18 18:34:07 -0300

    Construtores e Construtores Opcionais

--- dart/Orient_Obj/classe_data.dart
+++ dart/Orient_Obj/classe_data.dart
@@ -3,6 +3,22 @@ class Data {
   int? mes;
   int? ano;
 
+  // Data(int dia, int mes, int ano) {
+  //   this.dia = dia;
+  //   this.mes = mes;
+  //   this.ano = ano;
+  // }
+
+  //ou
+
+  // Data(this.dia, this.mes, this.ano); //construtor obrigatorio
+
+  Data([
+    this.dia = 00,
+    this.mes = 00,
+    this.ano = 0000,
+  ]); //construtor opcional com valor default
+
   String obterFormatada() {
     return "$dia/$mes/$ano";
   }
@@ -13,13 +29,13 @@ class Data {
 }
 
 main() {
-  var dataAniversario = new Data();
-  dataAniversario.dia = 3;
-  dataAniversario.mes = 10;
-  dataAniversario.ano = 2020;
+  var dataAniversario = new Data(3, 10, 2020);
+  // dataAniversario.dia = 3;
+  // dataAniversario.mes = 10;
+  // dataAniversario.ano = 2020;
 
-  Data dataCompra = Data();
-  dataCompra.dia = 23;
+  Data dataCompra = Data(1, 1, 1970);
+  // dataCompra.dia = 23;
   dataCompra.mes = 12;
   dataCompra.ano = 2021;
 
@@ -32,4 +48,11 @@ main() {
 
   print(dataCompra);
   print(dataCompra.toString());
+
+  print('\nParametros opcionais\n');
+
+  print(new Data());
+  print(new Data(31));
+  print(new Data(31, 12));
+  print(new Data(31, 12, 2020));
 }
